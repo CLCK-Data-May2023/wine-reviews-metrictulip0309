69ead2ab0efd922f51267f06434c55d9fa42c1bf
metrictulip0309 <eclairez995@gmail.com>
2023-09-13 18:43:23 -0400

    reviews

--- reviews.py
+++ reviews.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 #read in data
-df = pd.read_csv('./data/winemag-data-130k-v2.csv')
+df = pd.read_csv('data/winemag-data-130k-v2.csv.zip', index_col=0)
 
 #total count
 rev_count = df.country.value_counts()
